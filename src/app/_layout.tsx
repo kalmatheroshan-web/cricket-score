49de6b2c2a748b738ad9eff638ff4a679199aa1e
Roshan-trial <iambackganesha@gmail.com>
2026-07-19 21:22:24 +0530

    all import

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,17 +11,4 @@ export default function Layout() {
     <Toast config={toastConfig} />
   </Provider>
   );
-}import { Stack } from "expo-router";
-import Toast from "react-native-toast-message";
-import { toastConfig } from "@/lib/toastconfig";
-import { Provider } from "react-redux";
-import { store } from "../hooks/store";
-import "./global.css";
-
-export default function Layout() {
-  return (<Provider store={store}>
-    <Stack screenOptions={{ headerShown: false }} />
-    <Toast config={toastConfig} />
-  </Provider>
-  );
 }
